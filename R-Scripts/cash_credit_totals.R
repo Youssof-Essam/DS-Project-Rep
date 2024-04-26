ab862e4d0045e7d66b199bb6f6f6a7e6223f8c4a
Youssof-Essam <150634244+Youssof-Essam@users.noreply.github.com>
2024-04-26 13:16:33 +0300

    Minor fix in cash and credit totals

--- R-Scripts/cash_credit_totals.R
+++ R-Scripts/cash_credit_totals.R
@@ -12,6 +12,12 @@ df<-read.csv("Datasets/all_cities.csv")
 
 # displays pie chart of payment totals per city
 cities<- sort(unique(df$city))
+
+totals<-c(sum(df$total[ tolower(df$paymentType) == "cash"]),sum(df$total[ tolower(df$paymentType) == "credit"]))
+#par(mar=c(0,0,2,0))
+chart<- pie(totals, paste(round(totals/sum(totals)*100,2),"%"),main="All cities",col = c("red","blue"))
+legend("bottomright",payment,fill = c("red","blue"))
+
 for (citiy in cities) {
   by_city<-df[df$city==citiy,]
   totals<-c(sum(by_city$total[ tolower(by_city$paymentType) == "cash"]),sum(by_city$total[ tolower(by_city$paymentType) == "credit"]))
@@ -20,9 +26,4 @@ for (citiy in cities) {
   chart<- pie(totals, paste(round(totals/sum(totals)*100,2),"%"),main=citiy,col = c("red","blue"))
   legend("bottomright",payment,fill = c("red","blue"))
 }
-totals<-c(sum(df$total[ tolower(df$paymentType) == "cash"]),sum(df$total[ tolower(df$paymentType) == "credit"]))
-#par(mar=c(0,0,2,0))
-chart<- pie(totals, paste(round(totals/sum(totals)*100,2),"%"),main="All cities",col = c("red","blue"))
-legend("bottomright",payment,fill = c("red","blue"))
-
 
